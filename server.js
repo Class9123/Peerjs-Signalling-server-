a73170795e4dfec7d73fa49a5685d64b544b56fb
Class9123 <jayjaygovind4@gmail.com>
2025-03-06 07:53:49 +0545

    Added 5000 as the deafault port

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
-const express = require('express');
 const { PeerServer } = require('peer');
 
-const app = express();
-const logs = [];
-
-// Create the PeerJS server
+const DEFAULT_PORT = 5000
 const peerServer = PeerServer({
-  port: process.env.PEER_PORT || 5000, // Use dynamic port binding for Render
+  port: process.env.PORT || DEFAULT_PORT, // Use dynamic port binding for Render
   path: '/myapp'
 });
 
 peerServer.on('connection', (client) => {
   const log = `Client connected: ${client.getId()} at ${new Date().toISOString()}`;
   console.log(log);
-  logs.push(log);
+  
 });
 
 peerServer.on('disconnect', (client) => {
   const log = `Client disconnected: ${client.getId()} at ${new Date().toISOString()}`;
   console.log(log);
-  logs.push(log);
 });
 
-// Route to display logs
-app.get('/log', (req, res) => {
-  res.json(logs);
-});
 
-// Start the Express server
-const PORT = process.env.EXPRESS_PORT || 9000;
-// app.listen(PORT, () => {
-//   console.log(`Express server running on port ${PORT}`);
-// }); 
-
-console.log('PeerJS signaling server running on port', process.env.PEER_PORT || 5000);
+console.log('PeerJS signaling server running on port', process.env.PORT || DEFAULT_PORT);
