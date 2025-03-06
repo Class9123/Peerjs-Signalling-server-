125e48ed5f93436b2817c184e2f778c0cada92cb
Class9123 <jayjaygovind4@gmail.com>
2025-03-06 09:43:14 +0545

    Last try

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ var server = require('http').createServer(app);
 
 app.use('/peerjs', ExpressPeerServer(server, options));
 const PORT = process.env.PORT || 5000
-server.listen(PORT, "localhost", () => {
+server.listen(PORT, "0.0.0.0",() => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
