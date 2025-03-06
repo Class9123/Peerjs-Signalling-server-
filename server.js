883eb3e15304ce82b96219b595e76fff4ea20298
Class9123 <jayjaygovind4@gmail.com>
2025-03-06 07:39:21 +0545

    Solving the port issue with render

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const logs = [];
 
 // Create the PeerJS server
 const peerServer = PeerServer({
-  port: process.env.PORT || 5000, // Use dynamic port binding for Render
+  port: process.env.PEER_PORT || 5000, // Use dynamic port binding for Render
   path: '/myapp'
 });
 
@@ -28,9 +28,9 @@ app.get('/log', (req, res) => {
 });
 
 // Start the Express server
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.EXPRESS_PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Express server running on port ${PORT}`);
 });
 
-console.log('PeerJS signaling server running on port', 5000);
+console.log('PeerJS signaling server running on port', process.env.PEER_PORT || 5000);
